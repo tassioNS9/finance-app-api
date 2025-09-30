e49098209e6ec951b451e584f52e1d8d852d970b
Tassio Neves <tassions@hotmail.com>
2025-09-30 07:37:34 -0300

    refactor: rename classes errors

--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -2,7 +2,7 @@ import { PostgresHelper } from '../../db/postgres/helper.js'
 
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        const results = await PostgresHelper.query(
+        await PostgresHelper.query(
             'INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
             [
                 createUserParams.id,
@@ -13,6 +13,11 @@ export class PostgresCreateUserRepository {
             ]
         )
 
-        return results[0]
+        const createdUser = await PostgresHelper.query(
+            'SELECT * FROM users WHERE id = $1',
+            [createUserParams.id]
+        )
+
+        return createdUser[0]
     }
 }
