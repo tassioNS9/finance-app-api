1de432f9468125a4caae7c961cee1bf79f84ae9d
Tassio Neves <tassions@hotmail.com>
2025-10-01 19:50:49 -0300

    feat: add GetUserByIdController and validations

---
+++ src/controllers/get-user-by-id.js
@@ -0,0 +1,24 @@
+import { ok, serverError, badRequest } from './helpers.js'
+import { GetUserByIdCase } from '../use-cases/get-user-by-id.js'
+import validator from 'validator'
+
+export class GetUserByIdController {
+    async execute(httpRequest) {
+        try {
+            const isIdValid = validator.isUUID(httpRequest.params.userId)
+
+            if (!isIdValid) {
+                return badRequest({ message: 'The provided id is not valid!' })
+            }
+            const getUserByIdUseCase = new GetUserByIdCase()
+
+            const user = await getUserByIdUseCase.execute(
+                httpRequest.params.userId
+            )
+            return ok(user)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
