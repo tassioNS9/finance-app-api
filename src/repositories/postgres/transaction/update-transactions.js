d700dc30f74d7239ce99e8298b8e0934ebf79b2b
Tassio Neves <tassions@hotmail.com>
2025-10-09 19:39:34 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transactions.js
@@ -0,0 +1,29 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionsRepository {
+    async execute(transactionId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(transactionId)
+
+        const updateQuery = `
+            UPDATE transactions
+            SET ${updateFields.join(', ')} 
+            WHERE id = $${updateValues.length}
+            RETURNING *
+        `
+
+        const updatedTransaction = await PostgresHelper.query(
+            updateQuery,
+            updateValues
+        )
+
+        return updatedTransaction[0]
+    }
+}
