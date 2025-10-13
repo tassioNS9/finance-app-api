b5dea74af237929a8a7a14d745767808e0076bc8
Tassio Neves <tassions@hotmail.com>
2025-10-13 20:48:27 -0300

    feat: add zod to CreateUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,13 +1,6 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js'
 import { badRequest, created, serverError } from '../helpers/http.js'
-import {
-    checkIfPasswordIsValid,
-    emailIsAlreadyInUseResponse,
-    checkIfEmailIsValid,
-    invalidPasswordResponse,
-} from '../helpers/user.js'
-import { validateRequiredFields } from '../helpers/validation.js'
-
+import { z, ZodError } from 'zod'
 export class CreateUserController {
     constructor(createUserUseCase) {
         this.createUserUseCase = createUserUseCase
@@ -16,38 +9,50 @@ export class CreateUserController {
         try {
             const params = httpRequest.body
 
-            const requiredFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
+            const createUserSchema = z.object({
+                first_name: z.string().trim().min(1, {
+                    message: 'First name is requireeed.',
+                }),
+                last_name: z
+                    .string({
+                        required_error: 'Last name is requireeed.',
+                    })
+                    .trim()
+                    .min(1, {
+                        message: 'Last name is required',
+                    }),
+                email: z
+                    .string({
+                        required_error: 'E-mail is required.',
+                    })
+                    .email({
+                        message: 'Please provide a valid e-mail.',
+                    })
+                    .trim()
+                    .min(1, {
+                        message: 'E-mail is required.',
+                    }),
+                password: z
+                    .string({
+                        required_error: 'Password is required.',
+                    })
+                    .trim()
+                    .min(6, {
+                        message: 'Password must have at least 6 characters.',
+                    }),
+            })
 
-            const { ok: requiredFieldsWereProvided, missingField } =
-                validateRequiredFields(params, requiredFields)
-
-            if (!requiredFieldsWereProvided) {
-                return badRequest({
-                    message: `The Field ${missingField} is required.`,
-                })
-            }
-
-            const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-            if (!passwordIsValid) {
-                return invalidPasswordResponse()
-            }
-
-            const emailIsValid = checkIfEmailIsValid(params.email)
-
-            if (!emailIsValid) {
-                return emailIsAlreadyInUseResponse()
-            }
+            await createUserSchema.parseAsync(params)
 
             const createdUser = await this.createUserUseCase.execute(params)
 
             return created(createdUser)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.message,
+                })
+            }
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message })
             }
