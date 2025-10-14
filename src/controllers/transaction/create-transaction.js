c0605142e12e876f770464b8404ebe352f3529c3
Tassio Neves <tassions@hotmail.com>
2025-10-14 13:41:24 -0300

    feat: add zod to CreateTransactionController

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -1,13 +1,15 @@
+import { ZodError } from 'zod'
+import { createTransactionSchema } from '../../schemas/transactions.js'
 import { badRequest, serverError, created } from '../helpers/http.js'
-import {
-    checkIfAmountIsValid,
-    checkIfTypeIsValid,
-} from '../helpers/transaction.js'
-import {
-    checkIfIdIsValid,
-    invalidIdResponse,
-    validateRequiredFields,
-} from '../helpers/validation.js'
+// import {
+//     checkIfAmountIsValid,
+//     checkIfTypeIsValid,
+// } from '../helpers/transaction.js'
+// import {
+//     checkIfIdIsValid,
+//     invalidIdResponse,
+//     validateRequiredFields,
+// } from '../helpers/validation.js'
 export class CreateTransactionController {
     constructor(createTransactionUseCase) {
         this.createTransactionUseCase = createTransactionUseCase
@@ -16,48 +18,52 @@ export class CreateTransactionController {
         try {
             const params = httpRequest.body
 
-            const requiredFields = ['user_id', 'name', 'date', 'amount', 'type']
+            // const requiredFields = ['user_id', 'name', 'date', 'amount', 'type']
 
-            const { ok: requiredFieldsWereProvided, missingField } =
-                validateRequiredFields(params, requiredFields)
+            // const { ok: requiredFieldsWereProvided, missingField } =
+            //     validateRequiredFields(params, requiredFields)
 
-            if (!requiredFieldsWereProvided) {
-                return badRequest({
-                    message: `The Field ${missingField} is required.`,
-                })
-            }
-            const userIdIsValid = checkIfIdIsValid(params.user_id)
+            // if (!requiredFieldsWereProvided) {
+            //     return badRequest({
+            //         message: `The Field ${missingField} is required.`,
+            //     })
+            // }
+            // const userIdIsValid = checkIfIdIsValid(params.user_id)
 
-            if (!userIdIsValid) {
-                return invalidIdResponse()
-            }
+            // if (!userIdIsValid) {
+            //     return invalidIdResponse()
+            // }
 
-            const amountIsValid = checkIfAmountIsValid(params.amount)
+            // const amountIsValid = checkIfAmountIsValid(params.amount)
 
-            if (!amountIsValid) {
-                return badRequest({
-                    message: 'The amount must be a valid currency value',
-                })
-            }
+            // if (!amountIsValid) {
+            //     return badRequest({
+            //         message: 'The amount must be a valid currency value',
+            //     })
+            // }
 
-            const type = params.type.trim().toUpperCase()
+            // const type = params.type.trim().toUpperCase()
 
-            const typeIsValid = checkIfTypeIsValid(type) // EARNING, EXPENSE, INVESTIMENT
+            // const typeIsValid = checkIfTypeIsValid(type) // EARNING, EXPENSE, INVESTIMENT
 
-            if (!typeIsValid) {
-                return badRequest({
-                    message: 'The type must be EARNING, EXPENSE or INVESTIMENT',
-                })
-            }
+            // if (!typeIsValid) {
+            //     return badRequest({
+            //         message: 'The type must be EARNING, EXPENSE or INVESTIMENT',
+            //     })
+            // }
+
+            await createTransactionSchema.parseAsync(params)
 
             const createdTransaction =
-                await this.createTransactionUseCase.execute({
-                    ...params,
-                    type,
-                })
+                await this.createTransactionUseCase.execute(params)
 
             return created(createdTransaction)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
             console.error(error)
             return serverError()
         }
