ef9ffe13809d46c4db1f15e48bf15b804b75d9ed
Tassio Neves <tassions@hotmail.com>
2025-10-15 10:29:10 -0300

    feat ensure Update and GetTransactionsByUserIdRepository use Prisma

--- src/repositories/postgres/transaction/update-transactions.js
+++ src/repositories/postgres/transaction/update-transactions.js
@@ -1,29 +1,14 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionsRepository {
     async execute(transactionId, updateTransactionParams) {
-        const updateFields = []
-        const updateValues = []
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateValues.length + 1}`)
-            updateValues.push(updateTransactionParams[key])
+        const updatedTransaction = await prisma.transaction.update({
+            where: {
+                id: transactionId,
+            },
+            data: updateTransactionParams,
         })
 
-        updateValues.push(transactionId)
-
-        const updateQuery = `
-            UPDATE transactions
-            SET ${updateFields.join(', ')} 
-            WHERE id = $${updateValues.length}
-            RETURNING *
-        `
-
-        const updatedTransaction = await PostgresHelper.query(
-            updateQuery,
-            updateValues
-        )
-
-        return updatedTransaction[0]
+        return updatedTransaction
     }
 }
