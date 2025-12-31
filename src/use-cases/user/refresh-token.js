790948c368904fe06ba32674d64bc788b305e9ed
Tassio Neves <tassions@hotmail.com>
2025-12-31 09:40:18 -0300

    feat: add RefreshTokenUseCase

---
+++ src/use-cases/user/refresh-token.js
@@ -0,0 +1,24 @@
+import { UnauthorizedError } from '../../errors/user'
+export class RefreshTokenUseCase {
+    constructor(tokensGeneratorAdapter, tokenVerifierAdapter) {
+        this.tokensGeneratorAdapter = tokensGeneratorAdapter
+        this.tokenVerifierAdapter = tokenVerifierAdapter
+    }
+
+    execute(refreshToken) {
+        try {
+            const decodedToken = this.tokenVerifierAdapter.execute(
+                refreshToken,
+                process.env.JWT_REFRESH_TOKEN_SECRET
+            )
+            if (!decodedToken) {
+                throw new UnauthorizedError()
+            }
+
+            return this.tokensGeneratorAdapter.execute(decodedToken.userId)
+        } catch (error) {
+            console.log(error)
+            throw new UnauthorizedError()
+        }
+    }
+}
