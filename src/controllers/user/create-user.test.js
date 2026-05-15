859fae058d9d29879fe6904e9a3023986c8ca285
Tassio Neves <tassions@hotmail.com>
2026-05-15 08:32:19 -0300

    test:ensure 400 is returned if email is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -57,4 +57,20 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'John Doe',
+                last_name: 'Doe',
+                password: 'password123',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
