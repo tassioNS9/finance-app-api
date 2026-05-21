c55f01df68c55aea1d97db6023ed6096471a4101
Tassio Neves <tassions@hotmail.com>
2026-05-21 10:19:34 -0300

    test:ensure DeleteTransactionController  returns 500  when DeleteTransactionUseCase throws an error

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -47,4 +47,16 @@ describe('Delete Transaction Controller', () => {
         // assert
         expect(result.statusCode).toBe(404)
     })
+
+    it('should return 500 when DeleteTransactionUseCase throws an error', async () => {
+        // arrange
+        const { sut, deleteTransactionUseCase } = makeSut()
+        jest.spyOn(deleteTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+        // act
+        const result = await sut.execute(httpRequest)
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
 })
