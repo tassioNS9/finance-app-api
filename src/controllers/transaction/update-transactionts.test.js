e925742883caa36c12c3463491d2a07575354be8
Tassio Neves <tassions@hotmail.com>
2026-05-21 12:15:26 -0300

    test:ensure UpdateTransactionController  returns 400 if  type is invalid

--- src/controllers/transaction/update-transactionts.test.js
+++ src/controllers/transaction/update-transactionts.test.js
@@ -71,4 +71,17 @@ describe('Update Transaction Controller', () => {
         })
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if type is invalid', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                type: 'invalid_type',
+            },
+        })
+        expect(result.statusCode).toBe(400)
+    })
 })
