fdf9638a7afb075880655ef8ecde71f1aa060391
Tassio Neves <tassions@hotmail.com>
2026-05-22 13:54:16 -0300

    test: ensure GetTransactionsByUserIdController returns 200 when finding user transactions

---
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -0,0 +1,53 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionsByUserIdController } from './get-transactions-by-user-id.js'
+
+describe('GetTransactionsByUserIdController', () => {
+    class GetTransactionsByUserIdUseCaseStub {
+        async execute() {
+            ;[
+                {
+                    id: faker.string.uuid(),
+                    user_id: faker.string.uuid(),
+                    name: faker.person.jobDescriptor(),
+                    date: faker.date.recent().toISOString(),
+                    type: faker.helpers.arrayElement([
+                        'EXPENSE',
+                        'EARNING',
+                        'INVESTMENT',
+                    ]),
+                    amount: faker.datatype.number(),
+                    description: 'Test transaction',
+                },
+            ]
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionsByUserIdUseCase =
+            new GetTransactionsByUserIdUseCaseStub()
+        const sut = new GetTransactionsByUserIdController(
+            getTransactionsByUserIdUseCase,
+        )
+        return { sut, getTransactionsByUserIdUseCase }
+    }
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+        query: {
+            from: faker.date.past().toISOString().split('T')[0],
+            to: faker.date.recent().toISOString().split('T')[0],
+        },
+    }
+
+    it('should return 200 when getting transactions successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(200)
+    })
+})
