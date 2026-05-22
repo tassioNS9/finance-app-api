20194291707e8ae395bbd13ee09e9718cf59beae
Tassio Neves <tassions@hotmail.com>
2026-05-22 16:30:10 -0300

    test: ensure DeleteTransactionController calls DeleteTransactionUseCase with correct params

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -70,4 +70,17 @@ describe('Delete Transaction Controller', () => {
         // assert
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call DeleteTransactionUseCase with correct transactionId', async () => {
+        // arrange
+        const { sut, deleteTransactionUseCase } = makeSut()
+        const executeSpy = jest.spyOn(deleteTransactionUseCase, 'execute')
+        // act
+        await sut.execute(httpRequest)
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(
+            httpRequest.params.transactionId,
+        )
+        expect(executeSpy).toHaveBeenCalledTimes(1)
+    })
 })
