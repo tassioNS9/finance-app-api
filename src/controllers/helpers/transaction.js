dfa29451a9ea47f8cd1ad6815c91544ea1d05558
Tassio Neves <tassions@hotmail.com>
2026-05-25 21:33:08 -0300

    refactor: remove unused code

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,19 +1,4 @@
-import validator from 'validator'
 import { notFound } from './http.js'
-
-export const checkIfAmountIsValid = (amount) => {
-    if (typeof amount !== 'number') {
-        return false
-    }
-    return validator.isCurrency(amount.toFixed(2), {
-        allow_negatives: false,
-        digits_after_decimal: [2],
-        decimal_separator: '.',
-    })
-}
-export const checkIfTypeIsValid = (type) => {
-    return ['EARNING', 'EXPENSE', 'INVESTIMENT'].includes(type)
-}
 
 export const transactionNotFoundResponse = () =>
     notFound({ message: 'Transaction not found.' })
--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,16 +1,4 @@
-import { badRequest, notFound } from './http.js'
-
-export const invalidPasswordResponse = () => {
-    return badRequest({
-        message: 'Password must be at least 6 characters',
-    })
-}
-
-export const emailIsAlreadyInUseResponse = () => {
-    return badRequest({
-        message: 'The provided e-mail is already in use',
-    })
-}
+import { notFound } from './http.js'
 
 export const userNotFoundResponse = () =>
     notFound({ message: 'User not found.' })
