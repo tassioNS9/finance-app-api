39aa1a8acc510d8580993eb013a6bc833ca6d0cf
Tassio Neves <tassions@hotmail.com>
2026-06-10 14:28:28 -0300

    test:ensure CreateTransactionUseCase creates transaction successfully

---
+++ src/use-cases/transaction/create-transaction.test.js
@@ -0,0 +1,72 @@
+import { CreateTransactionUseCase } from './create-transaction'
+import { faker } from '@faker-js/faker'
+describe('CreateTransactionUseCase', () => {
+    const user = {
+        first_name: faker.name.fullName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+            length: 8,
+        }),
+    }
+    const createTransactionParams = {
+        user_id: faker.datatype.uuid(),
+        name: faker.person.jobDescriptor(),
+        date: faker.date.recent().toISOString(),
+        type: faker.helpers.arrayElement(['EXPENSE', 'EARNING', 'INVESTMENT']),
+        amount: faker.datatype.number(),
+    }
+    class GetUserByIdRepositoryStub {
+        async execute(userId) {
+            return {
+                ...user,
+                id: userId,
+            }
+            // Simulate that a user exists with the given ID
+        }
+    }
+
+    class CreateTransactionRepositoryStub {
+        async execute(transactionData) {
+            return transactionData // Simulate successful transaction creation
+        }
+    }
+
+    class IdGeneratorAdapterStub {
+        execute() {
+            return 'random_id' // Simulate unique ID generation
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+        const createTransactionRepository =
+            new CreateTransactionRepositoryStub()
+        const idGeneratorAdapter = new IdGeneratorAdapterStub()
+
+        const sut = new CreateTransactionUseCase(
+            createTransactionRepository,
+            getUserByIdRepository,
+            idGeneratorAdapter,
+        )
+
+        return {
+            sut,
+            getUserByIdRepository,
+            createTransactionRepository,
+            idGeneratorAdapter,
+        }
+    }
+
+    it('should create a new transaction successfully', async () => {
+        const { sut } = makeSut()
+        // Arrange
+        const result = await sut.execute(createTransactionParams)
+
+        // Assert
+        expect(result).toEqual({
+            ...createTransactionParams,
+            id: 'random_id',
+        })
+    })
+})
