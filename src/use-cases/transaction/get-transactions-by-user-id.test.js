ace0237c3ede5ee7161cc9ec08d4f821c5c1a2bb
Tassio Neves <tassions@hotmail.com>
2026-06-30 09:37:23 -0300

    test: ensure GetTransactionsByUserIdUseCase throws UserNotFoundError if user doesn't exist

--- src/use-cases/transaction/get-transactions-by-user-id.test.js
+++ src/use-cases/transaction/get-transactions-by-user-id.test.js
@@ -1,71 +1,45 @@
 import { faker } from '@faker-js/faker'
 import { GetTransactionsByUserIdUseCase } from './get-transactions-by-user-id.js'
+import { UserNotFoundError } from '../../errors/user.js'
 
 describe('Get Transactions By User Id Use Case', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+            length: 7,
+        }),
+    }
+
+    class GetTransactionsByUserIdRepositoryStub {
+        async execute() {
+            return []
+        }
+    }
+
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+    const makeSut = () => {
+        const getTransactionsByUserIdRepository =
+            new GetTransactionsByUserIdRepositoryStub()
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+        const sut = new GetTransactionsByUserIdUseCase(
+            getTransactionsByUserIdRepository,
+            getUserByIdRepository,
+        )
+
+        return {
+            sut,
+            getTransactionsByUserIdRepository,
+            getUserByIdRepository,
+        }
+    }
     it('should return transactions by user id successfully', async () => {
-        const user = {
-            id: faker.datatype.uuid(),
-            first_name: faker.name.firstName(),
-            last_name: faker.name.lastName(),
-            email: faker.internet.email(),
-        }
-        const transactions = [
-            {
-                id: faker.string.uuid(),
-                user_id: user.id,
-                name: faker.person.jobDescriptor(),
-                date: faker.date.recent().toISOString(),
-                type: faker.helpers.arrayElement([
-                    'EXPENSE',
-                    'EARNING',
-                    'INVESTMENT',
-                ]),
-                amount: faker.finance.amount(),
-            },
-            {
-                id: faker.string.uuid(),
-                user_id: user.id,
-                name: faker.person.jobDescriptor(),
-                date: faker.date.recent().toISOString(),
-                type: faker.helpers.arrayElement([
-                    'EXPENSE',
-                    'EARNING',
-                    'INVESTMENT',
-                ]),
-                amount: faker.finance.amount(),
-            },
-        ]
-
-        class GetTransactionsByUserIdRepositoryStub {
-            async execute() {
-                return transactions
-            }
-        }
-
-        class GetUserByIdRepositoryStub {
-            async execute() {
-                return user
-            }
-        }
-
-        const makeSut = () => {
-            const getTransactionsByUserIdRepository =
-                new GetTransactionsByUserIdRepositoryStub()
-
-            const getUserByIdRepository = new GetUserByIdRepositoryStub()
-
-            const sut = new GetTransactionsByUserIdUseCase(
-                getTransactionsByUserIdRepository,
-                getUserByIdRepository,
-            )
-
-            return {
-                sut,
-                getTransactionsByUserIdRepository,
-                getUserByIdRepository,
-            }
-        }
-
         //arrange
         const { sut } = makeSut()
 
@@ -73,6 +47,34 @@ describe('Get Transactions By User Id Use Case', () => {
         const result = await sut.execute(user.id)
 
         //assert
-        expect(result).toEqual(transactions)
+        expect(result).toEqual([])
     })
+
+    it('should throw UserNotFoundError if user is not found', async () => {
+        //arrange
+        const { sut, getUserByIdRepository } = makeSut()
+        jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValueOnce(null)
+        const userId = faker.string.uuid()
+        //act
+        const promise = sut.execute(userId)
+
+        //assert
+        await expect(promise).rejects.toThrow(new UserNotFoundError(userId))
+    })
+
+    // it('should call GetUserByIdRepository with correct params', async () => {
+    //     //arrange
+    //     const { sut, getUserByIdRepository } = makeSut()
+    //     const getUserByIdRepositorySpy = jest.spyOn(
+    //         getUserByIdRepository,
+    //         'execute',
+    //     )
+    //     const userId = faker.string.uuid()
+    //     console.log('userId', userId)
+    //     //act
+    //     await sut.execute(userId)
+
+    //     //assert
+    //     expect(getUserByIdRepositorySpy).toHaveBeenCalledWith(userId)
+    // })
 })
