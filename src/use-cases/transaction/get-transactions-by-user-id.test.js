0b8d347ba024f6ec1b5b546ea3fd799780dea560
Tassio Neves <tassions@hotmail.com>
2026-06-30 09:48:32 -0300

    test: ensure GetTransactionsByUserIdUseCase throws if GetUserByIdRepository throws

--- src/use-cases/transaction/get-transactions-by-user-id.test.js
+++ src/use-cases/transaction/get-transactions-by-user-id.test.js
@@ -97,4 +97,18 @@ describe('Get Transactions By User Id Use Case', () => {
             to,
         )
     })
+
+    it('should throw if GetUserByIdRepository throws', async () => {
+        //arrange
+        const { sut, getUserByIdRepository } = makeSut()
+        jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+        const userId = faker.string.uuid()
+        //act
+        const promise = sut.execute(userId)
+
+        //assert
+        await expect(promise).rejects.toThrow()
+    })
 })
